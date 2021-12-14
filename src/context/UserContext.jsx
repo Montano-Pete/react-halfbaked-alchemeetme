0b689fa3ca591e14d3e9ec8a884e6786aa301312
Peter <pete225848@gmail.com>
2021-12-13 16:22:47 -0800

    added useEffect for fetching and settting User state to the UserProvider component

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,11 +1,22 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
+import { fetchUser } from '../services/user'
 
 const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState({})
 
-  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+  useEffect(() => {
+    fetchUser()
+      .then((fetchedUser) => {
+        setUser(fetchedUser)
+      })
+      .catch((error) => {
+        throw new Error(`Error: ${error}`)
+      })
+  }, [])
+
+  return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
 }
 
 const useUser = () => {
