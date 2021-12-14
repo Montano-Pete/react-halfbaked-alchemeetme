e7721a6b328aa1561f7416f46fb39d1ac1c45210
Peter <pete225848@gmail.com>
2021-12-13 16:57:49 -0800

    refactored appropriate files from using props to useContext hook

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
+import { UserProvider } from './context/UserContext'
 
 test('renders learn react link', () => {
-  render(<App />)
+  render(
+    <UserProvider>
+      <App />
+    </UserProvider>
+  )
 })
